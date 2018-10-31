9cd4d812ed9e6899e7939b36594a8adb30b718bc
Łukasz Bednarski <lukasy09@gmail.com>
2018-10-31 23:41:19 +0100

    assets done & readme

--- SimpleLinearRegression/model_tester.py
+++ SimpleLinearRegression/model_tester.py
@@ -1,7 +1,7 @@
 from linear_regression import SimpleLinearRegression
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
 data_path = "./data/Salary_Data.csv"
 dataframe =pd.read_csv(data_path)
 
@@ -11,7 +11,7 @@ Y = dataframe['Salary'].values
 model = SimpleLinearRegression()
 
 """Uncomment if you want to see the effect after 100000 epochs/iterations"""
-#model.fit(X, Y, epochs = 100000)
+model.fit(X, Y, epochs = 100000)
 #plt.figure(1, figsize = (8,5))
 #plt.scatter(X, Y)
 #plt.plot(X, model.predict(X))
@@ -26,8 +26,17 @@ def generate_model_progress(model,X, Y, save_path = "./generator_pictures/", max
         plt.figure(figsize = (8,5))
         plt.scatter(X, Y)
         plt.plot(X, model.predict(X))
-        plt.savefig(save_path + picture_prefix + str(epochs_number) + picture_sufix)
+        plt.savefig(save_path + str(epochs_number))
 
 """Uncomment if you want to generate plot for each epoch"""
 #generate_model_progress(model, X, Y)
 #print("Done")
+
+plt.figure(figsize=(8,5))
+plt.title("Dependency - Loss on epoch")
+plt.xlabel("epoch")
+plt.ylabel("loss")
+epoch_list = [i for i in range(0, 1000)]
+epoch_list = np.asarray(epoch_list)
+loss_vals = model.get_loss_values()
+plt.plot(epoch_list, loss_vals[0:1000])
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
-<p>
-	The repo contains implementations of popular Machine Learning algorithms & Learning technics.
-</p>
+<h1>Simple Linear Regression model</h1>
+
+<p>SLR enables to compute a model fitting 1-Dimensional train data. Below there is a presented graph that shows the efficiency of the model.</p>
+<p>Each point represents a single human - worker, X-axis his position at work, Y- axis his salary. The model tries to predict how much people should earn.</p>
+
+<p src = "./assets/SLR/slr.gif" align = "left"></p>
+<p src = "./assets/SLR/loss.png"></p>
