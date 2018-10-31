ec98cf6765db80ce0c5525713290e3f0cb7798c5
Łukasz Bednarski <lukasy09@gmail.com>
2018-10-31 20:44:53 +0100

    generator done

--- SimpleLinearRegression/model_tester.py
+++ SimpleLinearRegression/model_tester.py
@@ -9,8 +9,25 @@ X = dataframe['YearsExperience'].values
 Y = dataframe['Salary'].values
 
 model = SimpleLinearRegression()
-model.fit(X, Y, epochs = 10000)
 
-plt.scatter(X, Y)
-plt.ylim(20000, 12000)
-plt.plot(X, model.predict(X))
+"""Uncomment if you want to see the effect after 100000 epochs/iterations"""
+#model.fit(X, Y, epochs = 100000)
+#plt.figure(1, figsize = (8,5))
+#plt.scatter(X, Y)
+#plt.plot(X, model.predict(X))
+
+def generate_model_progress(model,X, Y, save_path = "./generator_pictures/", max_epoch = 1000):
+    model = SimpleLinearRegression()
+    picture_prefix = "pic"
+    picture_sufix = ".jpg"
+    for epochs_number in range(0, max_epoch):
+        model.fit(X, Y, epochs = epochs_number)
+        
+        plt.figure(figsize = (8,5))
+        plt.scatter(X, Y)
+        plt.plot(X, model.predict(X))
+        plt.savefig(save_path + picture_prefix + str(epochs_number) + picture_sufix)
+
+"""Uncomment if you want to generate plot for each epoch"""
+#generate_model_progress(model, X, Y)
+#print("Done")
